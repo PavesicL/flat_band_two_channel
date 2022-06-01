fixed bug with phase calculation

calc.py
@@ -38,7 +38,7 @@ def diagonalize_subspace(n, p):
 
 	if p.phase_fourier_transform:
 		mat, basis_transformation_matrix, phi_list = fourier_transform_matrix(mat, bas, p)
-		mat, basis_transformation_matrix = reorder_matrix_phi(mat, basis_transformation_matrix, phi_list)
+		#mat, basis_transformation_matrix = reorder_matrix_phi(mat, basis_transformation_matrix, phi_list) #THIS HAS A BUG! ALSO, IS NOT THAT IMPORTANT. REMOVE IT?
 	else:
 		basis_transformation_matrix = np.identity(len(bas))	
 

matrix.py
@@ -202,6 +202,7 @@ def reorder_matrix_phi(mat, basis_transformation_matrix, phi_list):
 	"""
 
 	print("Sorting matrix by phi.")
+	raise Exception("REORDERING BY PHI DOES NOT WORK! THE BASIS IS WRONG!")
 
 	sortingRule = [i for i in range(len(mat))]
 	zippedObj = sorted( list(zip(sortingRule, phi_list)), key = lambda x : x[1] )
@@ -211,6 +212,7 @@ def reorder_matrix_phi(mat, basis_transformation_matrix, phi_list):
 	#basis_transformation_matrix = reorder_matrix(basis_transformation_matrix, sortingRule)
 
 	basis_transformation_matrix = basis_transformation_matrix[:, sortingRule]
+	#basis_transformation_matrix = basis_transformation_matrix[sortingRule, :]
 
 	return mat, basis_transformation_matrix
 
@@ -221,7 +223,6 @@ def reorder_matrix(mat, rule):
 	"""
 	mat = mat[rule, :] [:, rule]	#numpy magic - reorders both columns and rows by the rule
 	return mat
-
 
 def unzip(zipped_object):	
 	unzipped = list(zip(*zipped_object))
@@ -252,17 +253,24 @@ def fourier_transform_basis(basis, p):
 		m_MAX = max(deltaMs)
 
 		phis = [ 2 * np.pi * i / (m_MAX +1) for i in range(len(deltaMs))]
-
+		print("PHIS")
+		print([ph/np.pi for ph in phis])
+		print("dMS")
+		print(deltaMs)
 		# now for each phi construct the vector |phi, QP>, by adding contributions e^(i phi dM) to the position of the eigenvector in the basis
 		for phi in phis:
 			for i, state in enumerate(basis):
 				if state.QP_state == QP:					
 					basis_transformation_matrix[i, total_count] += (1/np.sqrt(m_MAX + 1)) * cmath.exp( 1j * phi * 0.5 * (state.dM + m_MAX) )
 					
-			total_count += 1 #This counts which transformed vector |phi, QP> we are creating.
-			
-			all_phi_list.append(phi)	
+					aa = cmath.exp( 1j * phi * 0.5 * (state.dM + m_MAX) )
+					if state.nqp_no_imp == 0:
+						print("A", m_MAX, state, 0.5 * (state.dM + m_MAX), phi/np.pi, cmath.phase(aa)/np.pi)
 
+			all_phi_list.append(phi)
+			total_count += 1 #This counts which transformed vector |phi, QP> we are creating.
+	print("all phis:")
+	print(np.divide(all_phi_list, np.pi))		
 	if p.verbose:
 		P = basis_transformation_matrix
 		invP = np.transpose(np.conjugate(basis_transformation_matrix))
@@ -282,7 +290,7 @@ def fourier_transform_matrix(matrix, basis, p):
 	#invP = np.linalg.inv(P)
 	invP = np.transpose(np.conjugate(P))
 	mat = np.matmul(np.matmul(invP, matrix), P)
-
+	
 	return mat, P, phi_list
 
 ###################################################################################################
@@ -294,9 +302,8 @@ def check_state(state, n, p):
 		number of occupied levels is not larger than the number of all levels
 		number of cooper pairs is not negative
 	"""
-
 	if p.use_all_states:
-		return check_conditions( state.n == n, state.mL >= 0, state.mR >= 0 )
+		return check_conditions( state.n == n, state.mL >= 0, state.mR >= 0, state.nqp == 1)
 	else:
 		return check_conditions( [[bstate.n() == n, bstate.L.occupiedLevels() <= p.LL, bstate.R.occupiedLevels() <= p.LL, bstate.L.M >= 0, bstate.R.M >= 0] for bstate in state.basis_states] )
 

observables_calculation.py
@@ -18,7 +18,7 @@ def h5dump(file, saveString, values):
 # PRINT ENERGIES
 
 def print_and_save_energies(sector, n_dict, h5file, p):
-	print("\nEnergies: ")
+	print("Energies: ")
 	n, Sz = sector
 	
 	maxE = min(len(n_dict["energies"]), p.print_energies)
@@ -47,10 +47,12 @@ def print_state(eigenvector, basis, prec):
 	printList = []
 	for i, amplitude in enumerate(eigenvector):
 		if abs(amplitude) > prec:
-			printList.append([abs(amplitude), basis[i]])
-	printList = sorted(printList, key = lambda x : -abs(x[0]))
-	for amp, bas in printList:
-		print(f"{amp}	{bas}")		
+			size, phi = cmath.polar(amplitude)
+			printList.append([size, phi/np.pi, basis[i]])
+	#printList = sorted(printList, key = lambda x : -abs(x[0]))
+	printList = sorted(printList, key = lambda x : x[2].dM)
+	for amp, phi, bas in printList:
+		print(f"{amp}	{round(phi, 3)}	{bas}")		
 
 ###################################################################################################
 # OCCUPANCY CALCULATION
@@ -142,12 +144,14 @@ def calculate_phase(eigenvector, basis):
 	"""
 	This is equivalent to Eq. (3) form https://arxiv.org/pdf/cond-mat/0305361.pdf
 	"""
-	e_to_iphi = 0
+	e_to_iphi = 0 + 0 * 1j
 	for i, a_i in enumerate(eigenvector):
 		for j, a_j in enumerate(eigenvector):
 			if basis[i].QP_state == basis[j].QP_state:
-				e_to_iphi += a_i.conjugate() * a_j * delta(basis[i].mL, basis[j].mL + 1 ) * delta( basis[i].mR, basis[j].mR - 1)
-	
+
+				e_to_iphi +=  a_i.conjugate() * a_j * delta(basis[i].mL, basis[j].mL + 1 ) * delta( basis[i].mR, basis[j].mR - 1)
+				#e_to_iphi += 0.5 * a_i.conjugate() * a_j * delta(basis[i].mL, basis[j].mL - 1 ) * delta( basis[i].mR, basis[j].mR + 1)
+
 	size, phi = cmath.polar(e_to_iphi)
 	return size, phi
 
@@ -161,9 +165,9 @@ def print_and_save_all_phases(sector, h5file, states, basis):
 		h5dump(h5file, f"{n}/{Sz}/{i}/phi/", phi)
 		h5dump(h5file, f"{n}/{Sz}/{i}/phi_size/", size)
 
-		phis += f"{round(phi, 4)} "
+		phis += f"{round(phi/np.pi, 4)} "
 		sizes += f"{round(size, 4)} "
-	print(f"phi: {phis}")	
+	print(f"phi/pi: {phis}")	
 	print(f"phi size: {sizes}")
 
 ###################################################################################################
@@ -202,6 +206,8 @@ def process_save_and_print_results(d, h5file, p):
 		n_dict = d[sector]
 		energies, eigenstates, basis = n_dict["energies"], n_dict["eigenstates"], n_dict["basis"]
 
+		print()
+		print()
 		print(f"RESULTS FOR n = {n}, Sz = {Sz}:")
 		print_and_save_energies(sector, n_dict, h5file, p)
 		print_states(eigenstates, basis, p)