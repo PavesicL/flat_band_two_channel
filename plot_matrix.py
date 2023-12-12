1163a6644bb2199d0249dce5a6e9e5c708907db9
Luka Pavesic <luka.pavesic@ijs.si>
2023-12-12 15:46:57 +0100

    added examples

--- plot_matrix.py
+++ plot_matrix.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 
+import sys
 import numpy as np
 import matplotlib.pyplot as plt
 ###################################################################################################
 
-n = 16
-matName = f"matrix_n{n}"
+if len(sys.argv) != 2:
+	print("Usage: " + str(sys.argv[0]) + " matrix_file")
+	exit()
+matName = sys.argv[1]
 
 mat = np.genfromtxt(matName, dtype=complex)
 mat = np.abs(mat)
@@ -17,29 +20,12 @@ for i in mat:
 			countNonZero += 1
 print(f"Non zero elements count: {countNonZero}")
 
-if 1:
-	vmax = 2
-	plt.imshow(mat, cmap="Greys", vmin=0, vmax=vmax)
-	plt.title(fr"$n = {n}$, $vmax = {vmax}$")
-
-else:
-	antiDiagonal = [ mat[i][-i] for i in range(1, len(mat))]
-	xx = [i for i in range(len(mat)-1)]
-
-	print(antiDiagonal)
-
-	plt.plot(xx, antiDiagonal)
-
-print(len(mat), len(mat)/14)
-if 1:
-	ticks = [-0.5 + i * 14 for i in range(1+len(mat)//14)]
-	print(ticks)
-	plt.xticks(ticks)
-	plt.yticks(ticks)
+vmax = 2
+plt.imshow(mat, cmap="Greys", vmin=0, vmax=vmax)
+plt.title(fr"{matName}, $vmax = {vmax}$")
 
 plt.grid(zorder=50)
 
-#plt.title(fr"$n = {n}$")
-#plt.colorbar()
+plt.colorbar()
 plt.tight_layout()
-plt.show()
+plt.savefig(matName+"_plot.pdf")
